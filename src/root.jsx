893c56d02d0ed14245e6cb7379c34de1fbc82232
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-11-09 17:42:20 -0300

    Update root.jsx

--- src/root.jsx
+++ src/root.jsx
@@ -1,19 +1,59 @@
-import {useEffect, useState}  from "react"
-import esfera from "./4estrellas"
+import { useEffect, useState } from "react";
+import esfera from "./4es.png";
+import "./root.css";
 
-const Root = () => {
-    const [post,setPost] = useState('');
-    useEffect(()=>{
-        const postJSON = localStorage.getItem("post");
-        const postArray = JSON.parse(postJSON);
-        setPost(postArray);
-    });
-    return (
-        <>
-        <header>
-            <img src={esfera}></img>
-        </header>
-        </>
-    )
-}
+const Rotar = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <div className={`flip-container ${isFlipped ? 'flipped' : ''}`} onClick={handleFlip}>
+      <div className="flipper">
+        <div className="front">
+          <h1 style={{color:'black'}}>Hola</h1>
+        </div>
+        <div className="back">
+          <h1 style={{color:'black'}}>¡Rotado!</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Root = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  return (
+    <>
+      <header className="head">
+        <img src={esfera} className="ball" alt="Esfera del Dragón"></img>
+        <h1 className="dragon"><b>Dragon Ball WIKI</b></h1>
+      </header>
+      <div className="carousel-container">
+        <div className="image-slider" style={{ transform: `translateX(${-currentIndex * 100}%)` }}>
+          {images.map((image, index) => (
+            <div key={index} className="slide">
+              <img src={image} alt={`Imagen ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+        <div className="prev-btn" onClick={prevSlide}>&#9665;</div>
+        <div className="next-btn" onClick={nextSlide}>&#9655;</div>
+      </div>
+      <Rotar />
+    </>
+  );
+};
+
 export default Root;
