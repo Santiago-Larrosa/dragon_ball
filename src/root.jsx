f47637632e7e53e343cf13a73e42a7a14d0c283a
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-11-25 21:56:35 -0300

    Update root.jsx

--- src/root.jsx
+++ src/root.jsx
@@ -27,6 +27,7 @@ const series = [
     resume:
       "La historia sigue a un niño llamado Son Goku, quien vive solo en las montañas y tiene una cola de mono. Un día, conoce a Bulma, una brillante científica en busca de las Esferas del Dragón, siete esferas mágicas que, cuando se reúnen, invocan al dragón Shenlong, quien puede conceder un deseo.",
     year: "1988-1989",
+    a:"series",
   },
   {
     id: 2,
@@ -36,6 +37,7 @@ const series = [
     resume:
       "Dragon Ball Z (DBZ) es una serie de anime y manga japonesa creada por Akira Toriyama. Es la secuela de Dragon Ball y sigue las aventuras de Son Goku después de su victoria sobre Piccolo en el Torneo Mundial de Artes Marciales. La trama se centra en las batallas épicas de Goku y sus amigos contra poderosos enemigos, incluidos extraterrestres, androides y seres interdimensionales. ",
     year: "1989-1996",
+    a:"series",
   },
   {
     id: 3,
@@ -44,6 +46,7 @@ const series = [
     resume:" Dragon Ball GT es la continuación de Dragon Ball Z y sigue las aventuras de Son Goku en una búsqueda intergaláctica para recuperar las Esferas del Dragón dispersas. En esta emocionante travesía, Goku enfrenta nuevos desafíos cósmicos, conoce a nuevos aliados y enemigos, y explora dimensiones desconocidas en su esfuerzo por restaurar la paz en el universo.",
     name: "Dragon Ball GT",
     year:"1996-1997",
+    a:"series",
   },
   {
       id:4,
@@ -52,6 +55,7 @@ const series = [
       resume:" Dragon Ball Super continúa la épica saga de Goku y sus amigos después de los eventos de Dragon Ball Z. La trama se desarrolla tras la derrota de Majin Buu, introduciendo poderosos dioses y nuevos universos al multiverso de Dragon Ball. Goku y sus compañeros participan en torneos cósmicos, enfrentándose a enemigos formidables y desbloqueando niveles de poder aún más allá de su comprensión. Con animación moderna y emocionantes batallas, Dragon Ball Super lleva la franquicia a nuevas alturas.",
       name: "Dragon Ball Super",
       year:"2015-2018",
+      a:"series",
   },
   {
       id:5,
@@ -60,6 +64,7 @@ const series = [
       resume:"Dragon Ball Heroes es una serie derivada que se sumerge en un fascinante mundo de batallas interdimensionales y fusiones de personajes icónicos de Dragon Ball. Con un enfoque en la acción y los enfrentamientos épicos, la trama sigue a un grupo de héroes que se embarcan en misiones para proteger el multiverso. Presenta una narrativa única que aprovecha al máximo el vasto elenco de personajes y las posibilidades ilimitadas de la franquicia Dragon Ball, brindando a los fanáticos una experiencia llena de emoción y sorpresas.",
       name:"Dragon Ball Heroes",
       year:"2018",
+      a:"series",
   },
   {
       id:6,
@@ -68,44 +73,49 @@ const series = [
       resume:"Dragon Ball Daima nos transporta a una nueva aventura en el universo de Dragon Ball. Con una historia original, la serie nos sumerge en emocionantes batallas y desafíos mientras sigue las hazañas de nuevos personajes. Con un estilo único y giros inesperados, Dragon Ball Daima promete explorar nuevas dimensiones del universo Dragon Ball y cautivar a los fanáticos con su frescura y originalidad.",
       name:"Dragon Ball Daima",
       year:"2024",
+      a:"series",
   },
 ];
 const movies = [
   {
-    id: 1,
+    id:1,
     name: "La batalla de los dioses",
     image_front: BOG,
     image_back: BOGL,
     resume:
       "Los Guerreros Z se enfrentan a Beerus, el Dios de la Destrucción, quien busca un oponente digno. Goku debe alcanzar nuevas cotas de poder para detener esta amenaza divina.",
     year: "2013",
+    a:"movies",
   },
   {
-    id: 2,
+    id:2,
     name: "La resurrección de Freezer",
     image_front: ROF,
     image_back: ROFL,
     resume:
       "Freezer, el antiguo enemigo de Goku, es resucitado con un poder aún más formidable. Los Guerreros Z deben enfrentarse a esta amenaza para proteger la Tierra.",
     year: "2015",
+    a:"movies",
   },
   {
-    id: 3,
+    id:3,
     name: "Dragon Ball Super: Broly",
     //image_front: ,
     //image_back: ,
     resume:
       "La historia del pasado de Broly se revela cuando este poderoso Saiyan se cruza en el camino de Goku y Vegeta. Una batalla épica está a punto de desatarse.",
     year: "2018",
+    a:"movies",
   },
   {
-    id: 4,
+    id:4,
     name: "Dragon Ball Super: Hero",
     //image_front: ,
     //image_back: ,
     resume:
       "Goku y sus aliados enfrentan una nueva amenaza que pone en peligro la existencia del universo. Una historia épica llena de acción y nuevos desafíos.",
     year: "2023", 
+    a:"movies",
   },
 ];
 
@@ -125,7 +135,7 @@ const Rotar = ({ serie, isVisible }) => {
               <img src={serie.image_front} style={{ width: "100%" }} alt="Dragon Ball" />
               <h1 style={{margin:"20px"}}>{serie.name}</h1>
               <h3 style={{margin:"20px"}}>{serie.year}</h3>
-              <a style={{ color: "white", margin:"20px" }} href={`/series/${parseInt(serie.id)}`}><button style={{ backgroundColor: "orange" }}>Ver más</button></a>
+              <a style={{ color: "white", margin:"20px" }} href={`/${serie.a}/${parseInt(serie.id)}`}><button style={{ backgroundColor: "orange" }}>Ver más</button></a>
             </div>
           </div>
           <div className="back" >
@@ -155,11 +165,11 @@ const Rotar2 = ({ movies, isVisible }) => {
               <img src={movies.image_front} style={{ width: "100%" }} alt="Dragon Ball" />
               <h2  className="titulon" style={{margin:"20px"}}>{movies.name}</h2>
               <h3 style={{margin:"20px"}}>{movies.year}</h3>
-              <a style={{ color: "white", margin:"20px" }} href={`/series/${parseInt(serie.id)}`}><button style={{ backgroundColor: "orange" }}>Ver más</button></a>
+              <a style={{ color: "white", margin:"20px" }} href={`/movies/${parseInt(movies.id)}`}><button style={{ backgroundColor: "orange" }}>Ver más</button></a>
             </div>
           </div>
           <div className="back" >
-            <div className="atras" style={{ left: `${100 + parseInt(serie.id - 1) * 600}px`, position: "absolute", height: "800px" }} >
+            <div className="atras" style={{ left: `${100 + parseInt(movies.id - 1) * 600}px`, position: "absolute", height: "800px" }} >
               <img src={movies.image_back} style={{ width: "100%" }} alt="Dragon Ball" />
               <h3>{movies.resume}</h3>
             </div>
