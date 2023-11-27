21412f04588d80d2d28fbab0f60b7fdb3b5f0f9f
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-11-26 21:11:25 -0300

    Update series.jsx

--- src/series.jsx
+++ src/series.jsx
@@ -16,6 +16,9 @@ import DBB from "./DBBan.png"
 import DBZB from "./DBZBan.png"
 import DBHB from "./DBHBan.png"
 import DBDB from "./DBDBan.png"
+import T from "./twitter.png"
+import Y from "./youtube.png"
+import I from "./instagram1.png"
 
 export default function Series() {
   const { id } = useParams();
@@ -102,6 +105,12 @@ export default function Series() {
         <h1 className="dragon">
           <b className='dragon2'>Dragon Ball WIKI</b>
         </h1>
+        <a href="https://www.instagram.com/dragonballsuper/?hl=es" target="_blank">
+        <img src={I} className="instagram" ></img></a>
+        <a href="https://twitter.com/DB_official_en?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank">
+          <img src={T} className="twitter"></img></a>
+        <a href="https://www.youtube.com/@ToeiAnimationjp" target="_blank">
+          <img src={Y} className="youtube"></img></a>
       </header>
       <div className='sup'>
          <img className="supi"src={seriess.sup}></img>
