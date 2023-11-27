f34e2585d95b9b8564267fcca2edd246630bb468
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-11-26 21:18:35 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,8 +10,12 @@ import Movies from"./movies"
 import MoviesList from "./moviesList"
 import Series from"./series"
 import SeriesList from "./seriesList"
-//import Characters from"./characters"
+import Blog from "./blog.jsx"
+import New from "./new.jsx"
+import Coment from "./comment.jsx"
+import Characters from"./characters"
 import Root from "./root";
+import CharList from "./CharList.jsx"
 import Imagen3 from "./DBSSH.png"
 import Imagen4 from "./super.png"
 import Imagen1 from "./fondo_1.png"
@@ -31,6 +35,22 @@ element: <SeriesList/>,
 path:"/movieslist",
 element:<MoviesList/>,
 },
+{
+path:"/blog",
+element:<Blog/>,
+},
+{
+path:"/new",
+element:<New/>,
+},
+{
+path:"/coment/:id",
+element:<Coment/>,
+},
+{
+  path:"/charlist",
+  element:<CharList/>,
+},
 /*{
   path: "/games",
   element: <Games/>
@@ -43,11 +63,11 @@ element:<MoviesList/>,
   path: "/series/:id",
   element: <Series/>
 },
-/*
+
 {
-  path: "/characters",
+  path: "/characters/:id",
   element: <Characters/>
-},*/
+},
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
